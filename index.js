8db0348f53bd3fe9e6596d7f4b5c078ee0f5c754
BesanaLorenzo <Az-62947>
2023-03-08 09:48:20 +0100

    operazioni

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+ 
+app.use(cors());
+ 
+//-- http://localhost:3000/
+app.get('/', function (req, res) {
+  res.send('Hello World');
+})
+//-- http://localhost:3000/somma?a=4&b=6
+app.get('/somma', function (req, res) {
+    let a=0
+    let b=0
+    a= req.query.a
+    b= req.query.b
+    c=a -(-b)
+    console.log("Addizione: "+ c)
+    res.send("Addizione: "+ c)
+  })
+
+  app.get('/sottrazione', function (req, res) {
+    let a=0
+    let b=0
+    a= req.query.a
+    b= req.query.b
+    c=a-b
+    console.log("Sottrazione: "+ c)
+    res.send("Sottrazione: "+ c)
+  })
+
+  app.get('/moltiplicazione', function (req, res) {
+    let a=0
+    let b=0
+    a= req.query.a
+    b= req.query.b
+    c=a *b
+    console.log("Moltiplicazione: "+ c)
+    res.send("Moltiplicazione: "+ c)
+  })
+
+  app.get('/divisione', function (req, res) {
+    let a=0
+    let b=0
+    a= req.query.a
+    b= req.query.b
+    c=a / b
+    console.log("Divisione: "+ c)
+    res.send("Divisione: "+ c)
+  })
+  
+app.listen(3000)
